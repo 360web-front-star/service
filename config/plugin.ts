e02e013696707b22870b20a76d40a15970c7fbca
hx <www.1567891@qq.com>
2018-05-10 21:43:38 +0800

    init

---
+++ config/plugin.ts
@@ -0,0 +1,12 @@
+import { EggPlugin } from "egg";
+
+const plugin: EggPlugin = {
+  // static: true,
+
+  ejs: {
+    enable: true,
+    package: "egg-view-ejs"
+  }
+};
+
+export default plugin;
---
+++ typings/app/controller/index.d.ts
@@ -0,0 +1,12 @@
+// This file was auto created by egg-ts-helper
+// Do not modify this file!!!!!!!!!
+
+import Edit from '../../../app/controller/edit';
+import Index from '../../../app/controller/index';
+
+declare module 'egg' {
+  interface IController {
+    edit: Edit;
+    index: Index;
+  }
+}
