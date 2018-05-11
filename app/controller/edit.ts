e92ccf80917aabaa86b406c530de2a2ec589ef55
hx <www.1567891@qq.com>
2018-05-11 16:39:21 +0800

    完成

--- app/controller/edit.ts
+++ app/controller/edit.ts
@@ -4,12 +4,14 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
 
-    const  type = ctx.request.query.template;
+    let type = ctx.request.query.template;
+    type = `/templates/${type}/template.ejs`;
 
     await ctx.render("edit/index.ejs", {
       data: {
         style: `/public/css/${type}.css`,
-        script: `/public/js/${type}.js`
+        script: `/public/js/${type}.js`,
+        type
       }
     });
   }
